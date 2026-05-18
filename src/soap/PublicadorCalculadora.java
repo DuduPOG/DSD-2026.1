058ae645535e134af934951593021bd8e3e09a78
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-05-18 17:14:14 -0300

    fix: mudando o ip para conectar corretamente com a aplicação

--- src/soap/PublicadorCalculadora.java
+++ src/soap/PublicadorCalculadora.java
@@ -6,7 +6,7 @@ import javax.xml.ws.Endpoint;
 public class PublicadorCalculadora {
     public static void main(String args[]){
         Endpoint.publish(
-            "http://10.24.22.180:9876/calcsoap", 
+            "http://10.25.1.21:9876/calcsoap", 
             new ServicoCalculadora()
         );
         System.out.println("Servico Calculadora em execucao...");
